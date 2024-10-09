9a79489ef1c0d857c55e3607adbf567b6fe3d0d8
hoceyn mohsenkhah <hoceyn@gmail.com>
2024-10-09 17:33:33 +0330

    [ fix ] Fix product card image animation
    Corrected the animation of the product card image by applying the
    `animate-onTheEarth` class to the container instead of the image
    itself. This ensures that the animation is smooth and accurate.
    Additionally, adjusted the gradient background to be placed
    correctly using the `rotate-180` class.
    
    Also, changed the condition for the `particular` class to
    correctly check for the `_featured` property being equal to 1.

--- cards/Product.tsx
+++ cards/Product.tsx
@@ -21,16 +21,18 @@ export default function Product({product}: { product: content }) {
 	return (
 		<Blocks.Gradient className={"relative mt-8 w-full"}>
 			<div className={"relative z-10 -my-8 text-center pb-10 px-4"}>
-				<div className={""}>
+				<div className={"animate-onTheEarth"}>
 					<div className={"w-3/4 mx-auto "} >
 						<Image src={localMedia} alt={product.title} type={"contain"}/>
 					</div>
 				</div>
-				<div className={"bg-gradient-shadow w-full h-12"}></div>
+				<div className={"rotate-180"}>
+					<div className={" animate-onTheEarth bg-gradient-shadow w-full h-12"}></div>
+				</div>
 				<H2 element={"h3"} className={"my-2"}>
 					{product.title}
 				</H2>
-				<Paragraph className={"text-slate-400"}>
+				<Paragraph className={"text-slate-400 h-9 text-ellipsis"}>
 					{product.short_description}
 				</Paragraph>
 				<div className={"my-4 w-full"}>
@@ -39,9 +41,9 @@ export default function Product({product}: { product: content }) {
 					</Button>
 				</div>
 			</div>
-			<div className={`absolute top-2 left-2 right-2 bottom-2 border border-indigo-500 rounded-2xl  ${product._featured ? "particular" : ""}`}>
+			<div className={`absolute top-2 left-2 right-2 bottom-2 border border-indigo-500 rounded-2xl  ${product._featured==1 ? "particular" : ""}`}>
 				{
-					product._featured &&
+					product._featured==1 &&
                     <>
                         <i></i>
                         <i></i>
