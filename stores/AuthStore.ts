67571ed924330caae79e875849e6f3ffb6ac3b19
Faeze <>
2024-11-11 15:27:30 +0330

    [fix] auth status in AuthStore

--- stores/AuthStore.ts
+++ stores/AuthStore.ts
@@ -1,35 +1,32 @@
-let authListener = [];
+let authListener: Function[] = [];
 let auth = false;
 
 if (typeof window !== 'undefined') {
-	// Ensure localStorage is only accessed in the client
-	auth = localStorage.getItem('auth') === 'true';
+  auth = localStorage.getItem('auth') === 'true';
 }
 
 export const AuthStores = {
-	setAuth(status) {
-		auth = status;
-		if (typeof window !== 'undefined') {
-			localStorage.setItem('auth', status);
-		}
-		emitAuthChange();
-	},
-	subscribe(listener) {
-		authListener = [...authListener, listener];
-		return () => {
-			authListener = authListener.filter(l => l !== listener);
-		};
-	},
-	getSnapshot() {
-		return auth;
-	},
-	getServerSnapshot: () => {
-		return auth;
-	}
+  setAuth(status: boolean) {
+    auth = status;
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('auth', status.toString()); 
+    }
+    emitAuthChange(); 
+  },
+  subscribe(listener: Function) {
+    authListener = [...authListener, listener];
+    return () => {
+      authListener = authListener.filter((l) => l !== listener);
+    };
+  },
+  getSnapshot() {
+    return auth;
+  },
+  getServerSnapshot: () => {
+    return auth;
+  },
 };
 
 function emitAuthChange() {
-	for (let listener of authListener) {
-		listener();
-	}
+  authListener.forEach((listener) => listener());
 }
