9695df55a85459fa5a6d6bb04f650865fdb2a37c
Faeze <>
2024-11-24 17:13:28 +0330

    [fix] default check

--- Switch.tsx
+++ Switch.tsx
@@ -8,7 +8,7 @@ interface switchProps {
 const Switch:React.FC<switchProps> = ({...props}) => {
     return (
         <label className={`relative w-14 h-8 rounded-full overflow-hidden flex items-center p-1 cursor-pointer`}>
-            <input {...props} className={`opacity-0 w-0 overflow-hidden h-0 peer cursor-pointer`} type={"checkbox"}
+            <input defaultChecked={props.defualtchecked} {...props} className={`opacity-0 w-0 overflow-hidden h-0 peer cursor-pointer`} type={"checkbox"}
                    id={`switch_${props.name}`}/>
             <div
                 className={"absolute w-full h-full left-0 top-0 bg-slate-700 peer-checked:bg-slate-900 transition-all"}></div>
