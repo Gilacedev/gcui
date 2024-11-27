e88596d0a117bad89667ff162eca0baf165b9c68
Faeze <>
2024-11-27 16:28:39 +0330

    [add] notification to navbars

--- ActionBar.tsx
+++ ActionBar.tsx
@@ -1,48 +1,103 @@
 "use client";
 
-
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 import Badge from "@/components/Badge";
 import ColorTypes from "@/components/functions/ColorTypes";
 import Language from "@/locales/Language";
 import ActionBarBasket from "@/components/ActionBarBasket";
-import { useEffect, useState, useSyncExternalStore } from 'react';
-import { AuthStores } from './stores/AuthStore';
-
+import { useEffect, useState, useRef, useSyncExternalStore } from "react";
+import { AuthStores } from "./stores/AuthStore";
+import Blocks from "./Blocks";
 
 const ActionBar = () => {
 	const language = Language("common");
 	const currentPath = usePathname();
 	const [authStatus, setAuthStatus] = useState(false);
-	const auth = useSyncExternalStore(AuthStores.subscribe, AuthStores.getSnapshot, AuthStores.getServerSnapshot);
+	const [isNotifOpen, setIsNotifOpen] = useState(false);
+	const notifRef = useRef<HTMLDivElement>(null);
+
+	const auth = useSyncExternalStore(
+		AuthStores.subscribe,
+		AuthStores.getSnapshot,
+		AuthStores.getServerSnapshot
+	);
 
 	useEffect(() => {
-		setAuthStatus(auth)
-	}, [auth])
+		setAuthStatus(auth);
+	}, [auth]);
+
+	const handleToggleNotif = () => {
+		setIsNotifOpen((prev) => !prev);
+	};
+
+	// Handle click outside to close the notification submenu
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (notifRef.current && !notifRef.current.contains(event.target as Node)) {
+				setIsNotifOpen(false);
+			}
+		};
+
+		document.addEventListener("mousedown", handleClickOutside);
+		return () => {
+			document.removeEventListener("mousedown", handleClickOutside);
+		};
+	}, []);
 
 	const items = [
-		{ path: '/dashboard/businesses', icon: 'fa fa-briefcase', label: language.businesses },
-		{ path: '/dashboard/invoices', icon: 'fa fa-credit-card', label: language.invoices },
-		{ path: '/dashboard/invoices', icon: 'fa fa-bell', label: language.events, hasBadge: true, badgeCount: 2 },
-		{ path: '/dashboard/support', icon: 'fa fa-headset', label: language.support },
+		{ path: "/dashboard/businesses", icon: "fa fa-briefcase", label: language.businesses },
+		{ path: "/dashboard/invoices", icon: "fa fa-credit-card", label: language.invoices },
+		{
+			path: "/dashboard/invoices",
+			icon: "fa fa-bell",
+			label: language.events,
+			hasBadge: true,
+			badgeCount: 2,
+		},
+		{ path: "/dashboard/support", icon: "fa fa-headset", label: language.support },
 	];
+
 	if (authStatus) {
 		return (
 			<div className="fixed bottom-2 left-4 rounded-2xl w-[calc(100dvw-2rem)] bg-violet-900 bg-opacity-20 backdrop-blur-3xl h-16 text-slate-300 z-40">
 				<ul className="flex justify-stretch">
 					{items.map((item, index) => (
-						<li key={index} className="w-1/5 border-l border-slate-300 border-opacity-10 relative">
+						<li
+							key={index}
+							className="w-1/5 border-l border-slate-300 border-opacity-10 relative"
+							ref={item.icon === "fa fa-bell" ? notifRef : undefined} // Assign ref to the notification icon
+						>
 							<a
 								href={item.path}
-								className={`flex gap-2 flex-col items-center justify-center h-16 ${currentPath === item.path ? 'border-b-4 border-violet-400' : ''
+								className={`flex gap-2 flex-col items-center justify-center h-16 ${currentPath === item.path ? "border-b-4 border-violet-400" : ""
 									}`}
+								onClick={item.icon === "fa fa-bell" ? (e) => { e.preventDefault(); handleToggleNotif(); } : undefined}
 							>
-								<span className={`${item.icon} ${currentPath === item.path ? 'text-violet-400' : 'text-slate-400'}`}></span>
+								<span
+									className={`${item.icon
+										} ${currentPath === item.path ? "text-violet-400" : "text-slate-400"}`}
+								></span>
 								<span className="text-xs">{item.label}</span>
 							</a>
 							{item.hasBadge && (
 								<div className="absolute -top-2 left-1/2">
 									<Badge color={ColorTypes.danger}>{item.badgeCount}</Badge>
+								</div>
+							)}
+							{/* Notification submenu */}
+							{isNotifOpen && item.icon === "fa fa-bell" && (
+								<div
+									className={`absolute bottom-16 left-1/2 transform -translate-x-1/2 w-64 shadow-lg rounded-lg transition-transform duration-500 ease-out ${isNotifOpen
+											? "scale-100 opacity-100"
+											: "scale-90 opacity-0"
+										}`}
+								>
+									<div className="bg-slate-800 rounded-2xl p-4">
+										<div className="p-4 border-b">
+											<p className="font-semibold text-slate-800">Notifications</p>
+										</div>
+										
+									</div>
 								</div>
 							)}
 						</li>
@@ -54,9 +109,8 @@ const ActionBar = () => {
 			</div>
 		);
 	} else {
-		return (<></>)
+		return <></>;
 	}
-
 };
 
 export default ActionBar;
--- ActionBarDesktop.tsx
+++ ActionBarDesktop.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useSyncExternalStore } from "react";
 import { AuthStores } from "@/components/stores/AuthStore";
 import MenuBasketButton from "@/components/MenuBasketButton";
@@ -7,50 +7,110 @@ import ColorTypes from "@/components/functions/ColorTypes";
 import Badge from "@/components/Badge";
 import Language from "@/locales/Language";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import Blocks from "./Blocks";
 
 const ActionBarDesktop = () => {
-	const authStatus = useSyncExternalStore(AuthStores.subscribe, AuthStores.getSnapshot, AuthStores.getServerSnapshot);
-	const router = useRouter();
+  const authStatus = useSyncExternalStore(
+    AuthStores.subscribe,
+    AuthStores.getSnapshot,
+    AuthStores.getServerSnapshot
+  );
+  const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
 
-	const [isClient, setIsClient] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
 
-	useEffect(() => {
-		setIsClient(true);
-	}, []);
+  const [isClient, setIsClient] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
 
-	if (!isClient) {
-		return null;
-	}
+  useEffect(() => {
+    setIsClient(true);
 
-	return (
-		<div className={"flex gap-2"}>
-			{authStatus && (
-				<div className={"flex gap-2"}>
-					<MenuBasketButton />
-					<div className={"relative"}>
-						<Button color={ColorTypes.default} tag={"a"} href={"/logout"} icon={<span className={"far fa-bell"} />} />
-						<div className={"absolute -top-2  left-1/2  "}>
-							<Badge color={ColorTypes.danger}>3</Badge>
-						</div>
-					</div>
-					<div>
-						<Button color={ColorTypes.default} onClick={() => {
-							router.replace("/management")
-						}} icon={<span className={"far fa-user-gear"} />} />
-					</div>
-				</div>
-			)}
-			{!authStatus && (
-				<div>
-					<Button color={ColorTypes.primary} tag={"a"} href={"/auth"}>
-						<span>{Language().login_to_panel}</span>
-						<span className={"far fa-users ms-2"} />
-					</Button>
-				</div>
-			)}
-		</div>
-	);
-}
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
+
+  return (
+    <div className={"flex gap-2"}>
+      {authStatus && (
+        <div className={"flex gap-2"}>
+          <MenuBasketButton />
+          <div className={"relative"} ref={menuRef}>
+            <Button
+              color={ColorTypes.default}
+              tag={"button"}
+              onClick={toggleMenu}
+              icon={<span className={"far fa-bell"} />}
+            />
+            <div className={"absolute -top-2 left-1/2"}>
+              <Badge color={ColorTypes.danger}>3</Badge>
+            </div>
+            {isOpen && (
+              <div
+                className={`absolute left-0 top-12 w-64 shadow-lg rounded-lg bg-opacity-100 transition-all duration-500 ease-out transform ${
+                  isOpen
+                    ? "scale-100 opacity-100"
+                    : "scale-90 opacity-0"
+                }`}
+              >
+                <div className="bg-slate-800 rounded-2xl p-4">
+                  <div className="p-4 border-b">
+                    <p className="font-semibold text-slate-50">Notifications</p>
+                  </div>
+                  <ul className="divide-y divide-gray-200">
+                    <li className="p-4 hover:bg-gray-100">Notification 1</li>
+                    <li className="p-4 hover:bg-gray-100">Notification 2</li>
+                    <li className="p-4 hover:bg-gray-100">Notification 3</li>
+                  </ul>
+                  <div className="p-4 text-center border-t">
+                    <a
+                      href="/notifications"
+                      className="text-blue-500 hover:underline"
+                    >
+                      View all
+                    </a>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+          <div>
+            <Button
+              color={ColorTypes.default}
+              onClick={() => {
+                router.replace("/management");
+              }}
+              icon={<span className={"far fa-user-gear"} />}
+            />
+          </div>
+        </div>
+      )}
+
+      {!authStatus && (
+        <div>
+          <Button color={ColorTypes.primary} tag={"a"} href={"/auth"}>
+            <span>{Language().login_to_panel}</span>
+            <span className={"far fa-users ms-2"} />
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default ActionBarDesktop;
