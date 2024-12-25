f0c21b662a8113811942c54e94c6df8ab9078407
Faeze <>
2024-12-25 17:04:42 +0330

    [fix] footer types

--- Footer.tsx
+++ Footer.tsx
@@ -4,29 +4,31 @@ import Blocks from "@/components/Blocks";
 import SettingType from "@/types/Setting";
 import Menu from "@/types/Menu";
 
-type FooterProps =  {
+type FooterProps = {
 	settings: SettingType[];
 	menu: Menu;
-	namads?: any; 
-  }
+	namads?: any;
+}
 
 const Footer: React.FC<FooterProps> = ({ settings, menu }) => {
 
 	let footerMenu = []
-	let footerMenuTitle = "" 
-	try {
-		// Ensure menu.meta is a string before parsing
-		if (typeof menu.meta === 'string') {
-		  let data = JSON.parse(menu.meta);
-		  footerMenu = data.menu;
-		  footerMenuTitle = menu.title ?? "";
-		} else {
-		  console.error("menu.meta is not a valid string");
+	let footerMenuTitle = ""
+	if (menu) {
+		try {
+			// Ensure menu.meta is a string before parsing
+			if (typeof menu.meta === 'string') {
+				let data = JSON.parse(menu.meta);
+				footerMenu = data.menu;
+				footerMenuTitle = menu.title ?? "";
+			} else {
+				console.error("menu.meta is not a valid string");
+			}
 		}
-	  }
-	  
-	catch (e) {
-		console.error("Footer menu error", e)
+
+		catch (e) {
+			console.error("Footer menu error", e)
+		}
 	}
 
 	return (
@@ -37,14 +39,14 @@ const Footer: React.FC<FooterProps> = ({ settings, menu }) => {
 						<div className={"flex items-center gap-2 flex-wrap"}>
 							<div className={"w-20"}>
 								<Image type={'contain'} src={"/assets/images/gilace-logo.svg"}
-									   alt={"gilace logo"}/>
+									alt={"gilace logo"} />
 							</div>
 							<div>
 								<div className={"font-bold text-3xl"}>
-									{settings.find((setting)=>setting?.name=="footer_site_name")?.value }
+									{settings.find((setting) => setting?.name == "footer_site_name")?.value}
 								</div>
 								<div className={"text-slate-700"}>
-									{settings.find((setting)=>setting?.name=="footer_site_slogan")?.value }
+									{settings.find((setting) => setting?.name == "footer_site_slogan")?.value}
 								</div>
 							</div>
 							<div className={"w-full text-sm text-slate-500"}>
@@ -60,17 +62,19 @@ const Footer: React.FC<FooterProps> = ({ settings, menu }) => {
 							</h2>
 							<ul className={"text-sm py-4 text-slate-500"}>
 								{
-									footerMenu && footerMenu.map((item: Menu) => {
-										return (
-											<li className={"py-1"} key={item.id}>
-												<a href={item.url ?? "#"} className={"flex items-center cursor-pointer hover:ps-2 transition-all"}>
-													<span className={`text-indigo-500 fa-${item.icon ?? "chevron-left"} fa`}></span>
-													<span className={"ps-1"}>
-														{item.title}
-													</span>
-												</a>
-											</li>
-										)
+									footerMenu && footerMenu.length > 0 && footerMenu.map((item: Menu) => {
+										if (item) {
+											return (
+												<li className={"py-1"} key={item.id}>
+													<a href={item.url ?? "#"} className={"flex items-center cursor-pointer hover:ps-2 transition-all"}>
+														<span className={`text-indigo-500 fa-${item.icon ?? "chevron-left"} fa`}></span>
+														<span className={"ps-1"}>
+															{item.title}
+														</span>
+													</a>
+												</li>
+											)
+										}
 									})
 								}
 							</ul>
@@ -85,14 +89,14 @@ const Footer: React.FC<FooterProps> = ({ settings, menu }) => {
 							<ul className={"text-sm py-4 text-slate-500 flex gap-4"}>
 								<li>
 									{
-										<a target={"_blank"} href={settings.find((setting)=>setting?.name=="instagram")?.value }>
+										<a target={"_blank"} href={settings.find((setting) => setting?.name == "instagram")?.value}>
 											<span className={"fa fab fa-instagram text-3xl"} />
 										</a>
 									}
 								</li>
 								<li>
 									{
-										<a target={"_blank"} href={settings.find((setting)=>setting?.name=="telegram")?.value }>
+										<a target={"_blank"} href={settings.find((setting) => setting?.name == "telegram")?.value}>
 											<span className={"fa fab fa-telegram text-3xl"} />
 										</a>
 									}
@@ -100,16 +104,16 @@ const Footer: React.FC<FooterProps> = ({ settings, menu }) => {
 								<li>
 									{
 										<a target={"_blank"}
-										   href={settings.find((setting) => setting?.name == "whatsapp")?.value}>
-											<span className={"fa fab fa-whatsapp text-3xl"}/>
+											href={settings.find((setting) => setting?.name == "whatsapp")?.value}>
+											<span className={"fa fab fa-whatsapp text-3xl"} />
 										</a>
 									}
 								</li>
 								<li>
 									{
 										<a target={"_blank"}
-										   href={settings.find((setting) => setting?.name == "youtube")?.value}>
-											<span className={"fa fab fa-youtube text-3xl"}/>
+											href={settings.find((setting) => setting?.name == "youtube")?.value}>
+											<span className={"fa fab fa-youtube text-3xl"} />
 										</a>
 									}
 								</li>
