a0b8e52807d8bf69e9c33af5ba38dfc05b2692b7
Faeze <>
2024-12-30 15:14:23 +0330

    [fix] loading in search

--- forms/Search.tsx
+++ forms/Search.tsx
@@ -3,14 +3,18 @@ import Language from "@/locales/Language";
 import Input from "@/components/Input";
 import { H2 } from "@/components/Typo";
 import { redirect, useSearchParams } from 'next/navigation'
+import { useState } from "react";
+import Loader from "../Loader";
 
 const Search = ({ show_title }: {
 	show_title?: boolean | true;
 }) => {
 	const searchParams = useSearchParams()
 	const keyword = searchParams.get('keyword')
+	const [loading, setLoading] = useState(false)
 
 	const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		setLoading(true)
 		e.preventDefault();
 		let data = new FormData(e.currentTarget);
 		let search = data.get("search");
@@ -33,23 +37,26 @@ const Search = ({ show_title }: {
 				<Input
 					defaultValue={keyword && `${keyword}`}
 					type="search"
-					icon={<button
-						onClick={(e) => {
-							onSubmit
-						}}>
-						<span
-							className="fas fa-search cursor-pointer"
-						>
-						</span>
-					</button>
+					icon={loading ?
+						<div className="w-8">
+							<Loader />
+						</div>
+						:
+						<button
+							onClick={(e) => {
+								onSubmit
+							}}>
+							<span
+								className="fas fa-search cursor-pointer"
+							>
+							</span>
+						</button>
 					}
 					placeholder={Language().search}
 					name="search"
 				/>
 			</form>
 		</div>
-
-
 	);
 }
 export default Search;
