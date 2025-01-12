ce7ec92313cd36a64d6492867d827a840b63f32e
hoceyn mohsenkhah <hoceyn@gmail.com>
2025-01-12 19:04:01 +0330

    [fix] Improve Radio component styling
    Improved the styling of the custom radio button component.  The
    `w-4 h-4` class was removed from the main radio button div to
    allow for more consistent sizing across different screen sizes and
    resolutions. An extra div was added to improve visual spacing
    within the radio button element.

--- Radio.tsx
+++ Radio.tsx
@@ -15,15 +15,16 @@ const Radio: React.FC<RadioProps> = ({ name, children, defaultChecked, onChange 
     <Blocks.Dark>
       <label className={`flex items-center justify-start p-1 cursor-pointer`}>
         <input
-          defaultChecked={defaultChecked}
-          className={`opacity-0 w-0 overflow-hidden h-0 peer cursor-pointer`}
-          type="radio"
-          id={`radio_${name}`}
-          name={name}
-          onChange={onChange}
+            defaultChecked={defaultChecked}
+            className={`opacity-0 w-0 overflow-hidden h-0 peer cursor-pointer`}
+            type="radio"
+            id={`radio_${name}`}
+            name={name}
+            onChange={onChange}
         />
-        <div className={"w-4 h-4 rounded-full bg-slate-300 peer-checked:bg-indigo-500 peer-checked:shadow-lg peer-checked:shadow-indigo-500 transition-all"}>
+        <div className={"rounded-full bg-slate-300 peer-checked:bg-indigo-500 peer-checked:shadow-lg peer-checked:shadow-indigo-500 transition-all"}>
           <div className={"w-2 h-2 rounded-full bg-white mx-auto my-1 animate-ping opacity-20"}></div>
+          <div className={"w-4"}></div>
         </div>
         <div>{children}</div>
       </label>
