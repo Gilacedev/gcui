50c332133fc8b92c365b64e45b142804cbc5d6d6
Faeze <>
2025-01-20 13:22:27 +0330

    [fix] ActionBar => in mobile, if notification box be open when click on back button notification box closed automatic

--- ActionBar.tsx
+++ ActionBar.tsx
@@ -7,13 +7,11 @@ import ActionBarBasket from "@/components/ActionBarBasket";
 import { useEffect, useState, useRef, useSyncExternalStore } from "react";
 import { AuthStores } from "./stores/AuthStore";
 import NotificationBox from "./NotificationBox";
-import { getNotificationCount } from "@/models/NotificationModel";
 import TransitionLink from "@/components/TransitionLink";
-import Router from 'next/router';
+import { getNotificationCount } from "@/models/NotificationModel";
 
 const ActionBar = () => {
   const language = Language("common");
-  const router = useRouter();
   const currentPath = usePathname();
   const [authStatus, setAuthStatus] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -25,12 +23,13 @@ const ActionBar = () => {
     AuthStores.getSnapshot,
     AuthStores.getServerSnapshot
   );
+
   const fetchNotificationCount = async () => {
     setLoading(true);
     try {
       const dbNotif = await getNotificationCount();
       if (dbNotif !== null) {
-        setNotificationCount(dbNotif);
+        setNotificationCount(parseInt(dbNotif));
       }
     } catch (error) {
       console.error("Failed to fetch notification count:", error);
@@ -42,11 +41,10 @@ const ActionBar = () => {
   useEffect(() => {
     if (auth) {
       fetchNotificationCount();
-      // Set an interval to fetch notifications every 3 minutes
+      // Set an interval to fetch notifications every 5 minutes
       const intervalId = setInterval(() => {
         fetchNotificationCount();
-      }, 180000);
-
+      }, 300000);
       return () => {
         clearInterval(intervalId);
       };
@@ -75,68 +73,89 @@ const ActionBar = () => {
     };
   }, []);
 
-  // Close notification submenu when full URL changes
+  // Close notification submenu when the path changes
   useEffect(() => {
-    const handleRouteChange = () => {
-      setIsNotifOpen(false);
-    };
-    Router.events.on('routeChangeStart', handleRouteChange);
-    return () => {
-      Router.events.off('routeChangeStart', handleRouteChange);
-    };
-  }, [router, setIsNotifOpen]);
+    setIsNotifOpen(false);
+  }, [currentPath]); // Detects path changes using `usePathname`
 
   if (authStatus) {
     return (
       <div className="fixed bottom-2 left-4 rounded-2xl w-[calc(100dvw-2rem)] bg-violet-900 bg-opacity-20 backdrop-blur-3xl h-16 text-slate-300 z-40">
         <ul className="grid grid-cols-5">
-          <li className="">
-            <TransitionLink href={"/management/business"}
-              className={`flex gap-2 flex-col items-center justify-center h-16 ${currentPath === "/management/business" ? "border-b-4 border-violet-400" : ""}`}>
+          <li>
+            <TransitionLink
+              href={"/management/business"}
+              className={`flex gap-2 flex-col items-center justify-center h-16 ${
+                currentPath === "/management/business" ? "border-b-4 border-violet-400" : ""
+              }`}
+            >
               <span
-                className={`fa fa-briefcase ${currentPath === "management/business" ? "text-violet-400" : "text-slate-400"}`} />
+                className={`fa fa-briefcase ${
+                  currentPath === "/management/business" ? "text-violet-400" : "text-slate-400"
+                }`}
+              />
               <span className="text-xs">{language.businesses}</span>
             </TransitionLink>
           </li>
           <li>
-            <TransitionLink href={"/management/invoice"}
-              className={`flex gap-2 flex-col items-center justify-center h-16 ${currentPath === "/management/invoice" ? "border-b-4 border-violet-400" : ""}`}>
+            <TransitionLink
+              href={"/management/invoice"}
+              className={`flex gap-2 flex-col items-center justify-center h-16 ${
+                currentPath === "/management/invoice" ? "border-b-4 border-violet-400" : ""
+              }`}
+            >
               <span
-                className={`fa fa-credit-card ${currentPath === "/management/invoice" ? "text-violet-400" : "text-slate-400"}`} />
+                className={`fa fa-credit-card ${
+                  currentPath === "/management/invoice" ? "text-violet-400" : "text-slate-400"
+                }`}
+              />
               <span className="text-xs">{language.invoices}</span>
             </TransitionLink>
           </li>
-          <li className={"relative"} onClick={(e) => {
-            e.preventDefault();
-            handleToggleNotif();
-          }} ref={notifRef}
+          <li
+            className={"relative"}
+            onClick={(e) => {
+              e.preventDefault();
+              handleToggleNotif();
+            }}
+            ref={notifRef}
           >
-            <div className={`flex gap-2 flex-col items-center justify-center h-16 ${isNotifOpen ? "border-b-4 border-violet-400" : ""}`}>
-              <span className={`fa fa-bell ${currentPath === "/dashboard/invoice" ? "text-violet-400" : "text-slate-400"}`} />
+            <div
+              className={`flex gap-2 flex-col items-center justify-center h-16 ${
+                isNotifOpen ? "border-b-4 border-violet-400" : ""
+              }`}
+            >
+              <span className={`fa fa-bell text-slate-400`} />
               <span className="text-xs">{language.events}</span>
             </div>
-            {
-              notificationCount && notificationCount > 0 &&
-              <div className={"absolute -top-2 left-1/2"}>
-                <Badge color={ColorTypes.primary}>{notificationCount}</Badge>
-              </div>
 
-            }
-
-            {
-              <div
-                className={`absolute bottom-16 left-1/2 transform -translate-x-1/2 w-72 rounded-lg transition-all duration-300 ease-out overflow-hidden ${isNotifOpen
+            <div
+              className={`absolute bottom-16 left-1/2 transform -translate-x-1/2 w-72 rounded-lg transition-all duration-300 ease-out overflow-hidden ${
+                isNotifOpen
                   ? "scale-100 opacity-100 translate-y-0"
                   : "scale-90 opacity-0 translate-y-8 pointer-events-none"
-                  }`} >
-                <NotificationBox isOpen={isNotifOpen} />
+              }`}
+            >
+              <NotificationBox isOpen={isNotifOpen} />
+            </div>
+            {notificationCount > 0 && (
+              <div className={"absolute top-0 right-0"}>
+                <Badge color={ColorTypes.primary}>{notificationCount}</Badge>
               </div>
-            }
+            )}
           </li>
           <li>
-            <TransitionLink href={"/management/ticket"}
-              className={`flex gap-2 flex-col items-center justify-center h-16 ${currentPath === "/management/ticket" ? "border-b-4 border-violet-400" : ""}`}>
-              <span className={`fa fa-headset ${currentPath === "/management/ticket" ? "text-violet-400" : "text-slate-400"}`} />
+            <TransitionLink
+              href={"/management/ticket"}
+              className={`flex gap-2 flex-col items-center justify-center h-16 ${
+                currentPath === "/management/ticket" ? "border-b-4 border-violet-400" : ""
+              }`}
+            >
+              <span
+                className={`fa fa-headset ${
+                  currentPath === "/management/ticket" ? "text-violet-400" : "text-slate-400"
+                }`}
+              />
               <span className="text-xs">{language.support}</span>
             </TransitionLink>
           </li>
