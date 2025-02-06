d6dfc4eaf534fa7e069bf9dd8988df892e1f7117
hoceyn mohsenkhah <hoceyn@gmail.com>
2025-02-06 15:27:55 +0330

    [add] Add product details and pricing pages
    Added dedicated pages for displaying product details and pricing plans.  The product details page now includes a breadcrumb trail, improved layout with images,  technical specifications link, and related projects section. The pricing page shows pricing plans for the selected product and provides a link to return to the product details page.  This enhances user experience by providing clear and organized product information.

---
+++ cards/Spec.tsx
@@ -0,0 +1,72 @@
+"use client"
+import Content from "@/types/Content";
+import Blocks from "@/components/Blocks";
+import Image from "@/components/Image";
+import Button from "@/components/Button";
+import ColorTypes from "@/components/functions/ColorTypes";
+import {ReactNode, useState} from "react";
+import HtmlContainer from "@/components/styles/HtmlContainer";
+
+type specProps = {
+	item:Content,
+	direction:"odd"|"even",
+	expand?:Function
+};
+const Spec = ({item,direction,expand}:specProps)=>{
+	const [open,setOpen] = useState(false)
+	if(!item)
+	{
+		return null;
+	}
+	let createdAt = new Date(item.created_at)
+
+
+	return (
+		<div className={`mt-8 sm:mt-2 sm:w-1/2 flex flex-col ${direction==="odd"?"sm:items-end sm:pe-4":"sm:items-start sm:start-1/2 sm:ps-4"} relative`}>
+			<div className={`absolute w-6 h-6 bg-violet-400  sm:translate-x-0 ${direction==="odd"?"-translate-x-1/2 end-1/2 sm:-end-3":"translate-x-1/2 start-1/2 sm:-start-3"} top-1 rounded-3xl`}></div>
+			<div className={"flex gap-1 items-center mb-3 "}>
+				<div className={"text-slate-500"}>
+					{createdAt.toLocaleDateString("fa-IR", {day:"2-digit"})}
+				</div>
+				<div className={"bg-slate-900 text-slate-400 p-2 rounded-lg"}>
+					{createdAt.toLocaleDateString("fa-IR", {month:"long"})}
+				</div>
+				<div className={"text-slate-500"}>
+					{createdAt.toLocaleDateString("fa-IR", {year:"numeric"})}
+				</div>
+			</div>
+			<Blocks.Dark className={"rounded-3xl bg-slate-700 p-4 w-full sm:w-2/3"}>
+				<div className={"text-amber-300 text-xl pb-3"}>
+					{item.title}
+				</div>
+				<div className={"text-xs leading-6"}>
+					{item.short_description}
+				</div>
+				<div className={`transition-all ${open?"max-h-screen overflow-y-scroll":"max-h-0 overflow-hidden"}`}>
+					<HtmlContainer html={item.description ??""} />
+				</div>
+				{
+					item && item._featured &&
+                    <div>
+						{
+							item.avatar &&
+							<div className={"py-8"}>
+								<Image src={process.env.NEXT_PUBLIC_UPLOAD_URL + "/" + item.avatar} alt={item.title}
+									   type={"contain"}></Image>
+							</div>
+						}
+                        <div className={"flex justify-end"}>
+                            <Button color={ColorTypes.default} onClick={(e) => {
+								setOpen(!open)
+							}}>
+								<span className={`far fa-chevron-down ${open && "rotate-180"}`}></span>
+                            </Button>
+                        </div>
+                    </div>
+
+				}
+			</Blocks.Dark>
+		</div>
+	);
+}
+export default Spec
