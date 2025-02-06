733a011ea8e0dc5c19a8e95d8391252d5bffd118
hoceyn mohsenkhah <hoceyn@gmail.com>
2025-02-06 15:26:55 +0330

    [ add ] Add breadcrumb component
    Added a breadcrumb component to display navigation paths.  This
    component renders a series of links representing the user's
    current location within the application, improving user
    experience and navigation clarity.  The component is
    configurable and supports icons and custom URLs.

---
+++ BreadCrumb.tsx
@@ -0,0 +1,29 @@
+import BreadCrumbType from "@/types/BreadCrumb";
+
+const BreadCrumb = ({items}:{items:BreadCrumbType[]})=>
+{
+	return (<div className={"flex bg-slate-900 bg-opacity-20 rounded-lg p-2 items-center text-sm text-slate-400 "}>
+		<span className={"text-violet-400"}></span>
+		{
+			items.map((item,index)=>{
+				return (<div>
+					<a href={item.url??""} className={" cursor-pointer hover:text-white flex items-center gap-1"} >
+						{
+							item.icon && <span className={`far fa-${item.icon}`}></span>
+						}
+						{
+							item.label &&
+							<span className={" bg-slate-900 bg-opacity-20 rounded-lg cursor-pointer hover:text-white"}>{item.label}</span>
+						}
+						{
+							index < items.length-1 && <span className={"far fa-chevron-left px-3"}></span>
+						}
+					</a>
+
+				</div>)
+			})
+		}
+	</div>)
+
+}
+export default BreadCrumb
