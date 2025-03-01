f40aa51219a09821731e7f018d6a939edec1c45a
hoceyn mohsenkhah <hoceyn@gmail.com>
2025-03-01 13:51:24 +0330

    [ improve ] Enhance SpecsList component
    Improved the SpecsList component to include pagination and
    loading indicators.  This enhances the user experience by
    allowing users to load more specs progressively instead of
    loading all at once, improving performance and usability.  The
    parallax effect has also been removed from the Spec component.

--- SpecsList.tsx
+++ SpecsList.tsx
@@ -1,15 +1,19 @@
 "use client"
-import {ParallaxProvider} from "react-scroll-parallax";
 import Spec from "@/components/cards/Spec";
 import Blocks from "@/components/Blocks";
-import {Paragraph} from "@/components/Typo";
 import Language from "@/locales/Language";
 import Input from "@/components/Input";
 import {ReactNode, useState} from "react";
 import Content from "@/types/Content";
+import Button from "@/components/Button";
+import {getRelated} from "@/models/ContentModel";
 
 const SpecsList = ({specs}:{specs:any}) =>{
 	const [localSpecs,setLocalSpecs] = useState(specs)
+	const [page,setPage] = useState(1)
+	const [loading,setLoading] = useState(false)
+	const [hasMore,setHasMore] = useState(true)
+
 	const localSearch = (e:React.ChangeEvent<HTMLInputElement>) =>{
 		let search = e.target.value
 		if(!search || search === "")
@@ -35,6 +39,19 @@ const SpecsList = ({specs}:{specs:any}) =>{
 		})
 		setLocalSpecs({...specs,data:newSpecs})
 	}
+	const loadMore = async () =>{
+		setLoading(true)
+		let newSpecs = await getRelated({slug: "products", content_type: "specs", page: page})
+		if(newSpecs.data.length === 0)
+		{
+			setHasMore(false)
+		}
+		setLocalSpecs({...specs,data:[...localSpecs.data,...newSpecs.data]})
+		setPage(page+1)
+		setLoading(false)
+	}
+
+
 	return(		<div>
 
 			<div className={"py-10 sm:block sm:w-96 sticky top-8 z-40"}>
@@ -42,7 +59,6 @@ const SpecsList = ({specs}:{specs:any}) =>{
 					<Input onInput={localSearch} className={""} icon={<span className={"far fa-search"} /> as ReactNode} placeholder={Language().specs_search_text}/>
 				</Blocks.Dark>
 			</div>
-			<ParallaxProvider>
 				<div className={"py-8 px-2 sm:px-0 relative overflow-x-hidden"}>
 					<div
 						className={"h-full w-4 rounded-3xl bg-violet-800/30 absolute top-12 left-1/2 -translate-x-1/2"}/>
@@ -58,8 +74,27 @@ const SpecsList = ({specs}:{specs:any}) =>{
 							})
 						}
 					</div>
+					<div>
+						{
+							loading &&
+							<div className={"text-center py-4"}>
+								<div className={"animate-spin inline-block w-6 h-6 bg-violet-300 rounded-full"}></div>
+							</div>
+						}
+						{
+							hasMore &&
+							<div className={"text-center py-4"}>
+								<Button tag={"button"} onClick={(e) => {
+									setLoading(true)
+									setPage(page + 1)
+									setLoading(false)
+								}} className={"bg-violet-300 text-slate-900 p-2 rounded-lg"}>
+									<span className={"far fa-chevron-down"}/>
+								</Button>
+							</div>
+						}
+					</div>
 				</div>
-			</ParallaxProvider>
 		</div>
 	);
 }
--- cards/Spec.tsx
+++ cards/Spec.tsx
@@ -6,7 +6,6 @@ import Button from "@/components/Button";
 import ColorTypes from "@/components/functions/ColorTypes";
 import {useState} from "react";
 import HtmlContainer from "@/components/styles/HtmlContainer";
-import {useParallax} from "react-scroll-parallax";
 
 type specProps = {
 	item:Content,
@@ -14,12 +13,6 @@ type specProps = {
 	expand?:Function
 };
 const Spec = ({item,direction,expand}:specProps)=>{
-	const parallax = useParallax<HTMLDivElement>({
-		easing:[0.6,0,0.45,1],
-		translateY:[0,-100],
-		opacity:[1,0.5]
-	});
-
 	const [open,setOpen] = useState(false)
 	if(!item)
 	{
@@ -29,7 +22,7 @@ const Spec = ({item,direction,expand}:specProps)=>{
 
 
 	return (
-		<div ref={parallax.ref} className={`not:first-child:opacity-10 mt-8 sm:mt-2 sm:w-1/2 flex flex-col ${direction==="odd"?"sm:items-end sm:pe-4":"sm:items-start sm:start-1/2 sm:ps-4"} relative`}>
+		<div  className={`not:first-child:opacity-10 mt-8 sm:mt-2 sm:w-1/2 flex flex-col ${direction==="odd"?"sm:items-end sm:pe-4":"sm:items-start sm:start-1/2 sm:ps-4"} relative`}>
 			<div className={`absolute w-6 h-6 bg-violet-300  sm:translate-x-0 ${direction==="odd"?"-translate-x-1/2 end-1/2 sm:-end-3":"translate-x-1/2 start-1/2 sm:-start-3"} top-1 rounded-3xl`}></div>
 			<div className={"flex gap-1 items-center mb-3 "}>
 				<div className={"text-slate-500"}>
