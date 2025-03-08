2ea05aa6fae676a95ef877f04229b33a322a26a4
Faeze <>
2025-03-08 10:42:33 +0330

    [fix] popup

--- Popup.tsx
+++ Popup.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { useEffect, useState } from "react";
-
 import Button from "./Button";
 import Language from "@/locales/Language";
 import Modal from "./Modal";
+import Image from "./Image";
 
 
 const Popup = () => {
@@ -14,8 +14,6 @@ const Popup = () => {
 
     useEffect(() => {
         const modalState = localStorage.getItem("hidePopup");
-        console.log("modalState:::", modalState);
-        
         if (modalState !== "true") {
             setTimeout(() => {
                 setIsOpen(true);
@@ -38,39 +36,41 @@ const Popup = () => {
         setIsOpen(false);
     };
 
-    if (!show) return null;
-
-    const lang = Language(); 
+    const lang = Language();
 
     return (
-        <Modal open={isOpen} onClose={handleClose} name="modal-popup" zindex={100}>
-            <div className="py-12 px-8 flex flex-col items-center justify-center">
-                <div className="py-4" />
-                <div className="p-5">
-                    <div className="flex flex-col items-center">
-                        <span className="font-bold text-white text-lg">جشنواره تخفیف بهاری</span>
-                        <span>توضیحات</span>
+        <div className="relative z-50">
+            <Modal open={isOpen} onClose={handleClose} name="modal-popup" zindex={50}>
+                <div className="py-12 px-8 flex flex-col items-center justify-center">
+                    <div className="py-4" />
+
+                    <div className="flex flex-col items-center gap-2">
+                        <Image src="" alt="" type="contain" />
+                        <span className="font-bold text-white text-lg mt-2">جشنواره تخفیف بهاری</span>
+                        <div className="flex items-center mt-4 gap-1">
+                            <input
+                                type="checkbox"
+                                id="doNotShowAgain"
+                                className="mr-2"
+                                checked={doNotShowAgain}
+                                onChange={(e) => setDoNotShowAgain(e.target.checked)}
+                            />
+                            <label htmlFor="doNotShowAgain" className="text-sm text-gray-200">
+                                دیگر نمایش داده نشود
+                            </label>
+                        </div>
+                        <div className="flex justify-center items-center w-full mt-5 cursor-pointer">
+                            <Button type="button" onClick={handleClose}>
+                                {lang.close}
+                            </Button>
+                        </div>
                     </div>
-                    <div className="flex items-center mt-4">
-                        <input
-                            type="checkbox"
-                            id="doNotShowAgain"
-                            className="mr-2"
-                            checked={doNotShowAgain}
-                            onChange={(e) => setDoNotShowAgain(e.target.checked)}
-                        />
-                        <label htmlFor="doNotShowAgain" className="text-sm text-gray-200">
-                            دیگر نمایش داده نشود
-                        </label>
-                    </div>
-                    <div className="flex gap-2 p-4">
-                        <Button type="button" onClick={handleClose}>
-                            {lang.close}
-                        </Button>
-                    </div>
+
+
+
                 </div>
-            </div>
-        </Modal>
+            </Modal>
+        </div>
     );
 };
 
