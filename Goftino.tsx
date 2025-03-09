a910f5464f95f2949059c67f36ede10b777b99d2
hoceyn mohsenkhah <hoceyn@gmail.com>
2025-03-09 17:16:53 +0330

    [add] Use NextImage component for better image optimization

---
+++ Goftino.tsx
@@ -0,0 +1,15 @@
+"use client";
+import Script from "next/script";
+
+const Goftino = () => {
+	return(
+		<Script
+				id="goftino-widget"
+				dangerouslySetInnerHTML={{
+					__html: `!function(){var i="nrLJRL",a=window,d=document;function g(){var g=d.createElement("script"),s="https://www.goftino.com/widget/"+i,l=localStorage.getItem("goftino_"+i);g.async=!0,g.src=l?s+"?o="+l:s;d.getElementsByTagName("head")[0].appendChild(g);}"complete"===d.readyState?g():a.attachEvent?a.attachEvent("onload",g):a.addEventListener("load",g,!1);}();`
+				}}
+				defer={true}
+			/>
+	)
+}
+export default Goftino
--- Image.tsx
+++ Image.tsx
@@ -24,8 +24,7 @@ const Image = ({src, alt, type ,priority}: ImageProps) => {
 			}}
 				className={`w-full h-full transition-all duration-1000 ${type === "cover" ? "object-cover" : "object-contain"} ${loading ? "opacity-0 scale-75" : "opacity-100 scale-100" }`}
 				src={src} alt={alt}/>
-			<div
-				className={`absolute border-2 border-slate-300/30 rounded-full particular w-6 h-6 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ${loading ? "block" : "hidden"}`}>
+			<div className={`absolute border-2 border-slate-300/30 rounded-full particular w-6 h-6 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ${loading ? "block" : "hidden"}`}>
 				<i/>
 			</div>
 		</div>
