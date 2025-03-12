8ba3f418b3ac1d3b0e1d71f1f142e92c8cf9c83b
Faeze <>
2025-03-12 13:06:43 +0330

    [fix] popup

--- Popup.tsx
+++ Popup.tsx
@@ -5,68 +5,95 @@ import Button from "./Button";
 import Language from "@/locales/Language";
 import Modal from "./Modal";
 import Image from "./Image";
+import ContentType from "@/types/ContentType";
+import { useRouter } from "next/navigation";
 
+type PopupProps = {
+    popups: ContentType;
+};
 
-const Popup = () => {
+const Popup: React.FC<PopupProps> = ({ popups }) => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
-    const [show, setShow] = useState<boolean>(false);
     const [doNotShowAgain, setDoNotShowAgain] = useState<boolean>(false);
+    const [currentPopup, setCurrentPopup] = useState<any>(null);
+    const router = useRouter();
 
     useEffect(() => {
-        const modalState = localStorage.getItem("hidePopup");
-        if (modalState !== "true") {
-            setIsOpen(true);
-        }
-    }, []);
+        const contents = popups?.contents ?? [];
+        const hiddenPopups = JSON.parse(localStorage.getItem("hiddenPopups") || "[]");
+        const firstActivePopup = contents.find(popup => popup?._status === 1 && !hiddenPopups.includes(popup.id));
 
-    useEffect(() => {
-        if (isOpen) {
-            setShow(true);
-        } else {
-            setShow(false);
+        if (firstActivePopup) {
+            setCurrentPopup(firstActivePopup);
+            setTimeout(() => {
+                setIsOpen(true);
+            }, 1000);
         }
-    }, [isOpen]);
+    }, [popups]);
+
 
     const handleClose = () => {
-        if (doNotShowAgain) {
-            localStorage.setItem("hidePopup", "true");
+        if (doNotShowAgain && currentPopup) {
+            const hiddenPopups = JSON.parse(localStorage.getItem("hiddenPopups") || "[]");
+            hiddenPopups.push(currentPopup.id);
+            localStorage.setItem("hiddenPopups", JSON.stringify(hiddenPopups));
         }
         setIsOpen(false);
     };
 
     const lang = Language();
 
+    const handleNavigate = () => {
+        if (currentPopup) {
+            const hiddenPopups = JSON.parse(localStorage.getItem("hiddenPopups") || "[]");
+            if (!hiddenPopups.includes(currentPopup.id)) {
+                hiddenPopups.push(currentPopup.id);
+                localStorage.setItem("hiddenPopups", JSON.stringify(hiddenPopups));
+            }
+            router.push(`/${currentPopup.slug}`);
+        }
+    };
+
     return (
         <div className="relative z-50">
-            <Modal open={isOpen} onClose={handleClose} name="modal-popup" zindex={50}>
-                <div className="py-12 px-8 flex flex-col items-center justify-center">
-                    <div className="py-4" />
-                    <div className="flex flex-col items-center gap-2">
-                        {/* <Image src="" alt="" type="contain" /> */}
-                        <span className="font-bold text-white text-lg mt-2">جشنواره تخفیف بهاری</span>
-                        <div className="flex items-center mt-4 gap-1">
-                            <input
-                                type="checkbox"
-                                id="doNotShowAgain"
-                                className="mr-2"
-                                checked={doNotShowAgain}
-                                onChange={(e) => setDoNotShowAgain(e.target.checked)}
-                            />
-                            <label htmlFor="doNotShowAgain" className="text-sm text-gray-200">
-                                دیگر نمایش داده نشود
-                            </label>
-                        </div>
-                        <div className="flex justify-center items-center w-full mt-5 cursor-pointer">
-                            <Button color="default" type="button" onClick={handleClose}>
-                                {lang.close}
-                            </Button>
+            {currentPopup && (
+                <Modal open={isOpen} onClose={handleClose} name="modal-popup" zindex={50}>
+                    <div className="py-5 flex flex-col items-center justify-center">
+                        <div className="flex flex-col items-center gap-2">
+                            <div onClick={handleNavigate} className="cursor-pointer rounded-md lg:w-96 w-64">
+                                {currentPopup.avatar && (
+                                    <Image src={process.env.NEXT_PUBLIC_UPLOAD_URL + "/" + currentPopup.avatar}
+                                        alt={currentPopup.title} type="cover" />
+                                )}
+                            </div>
+                            <span onClick={handleNavigate} className="cursor-pointer font-bold text-white text-lg mt-2">
+                                {currentPopup.title}
+                            </span>
+                            {currentPopup.short_description != "" &&
+                                <span onClick={handleNavigate} className=" text-slate-400 cursor-pointer text-sm mt-2">
+                                    {currentPopup.short_description}
+                                </span>}
+                            <div className="flex items-center mt-2 gap-1">
+                                <input
+                                    type="checkbox"
+                                    id="doNotShowAgain"
+                                    className="mr-2"
+                                    checked={doNotShowAgain}
+                                    onChange={(e) => setDoNotShowAgain(e.target.checked)}
+                                />
+                                <label htmlFor="doNotShowAgain" className="text-sm text-gray-200">
+                                    دیگر نمایش داده نشود
+                                </label>
+                            </div>
+                            <div className="flex justify-center items-center w-full mt-2 cursor-pointer">
+                                <Button color="default" type="button" onClick={handleClose}>
+                                    {lang.close}
+                                </Button>
+                            </div>
                         </div>
                     </div>
-
-
-
-                </div>
-            </Modal>
+                </Modal>
+            )}
         </div>
     );
 };
