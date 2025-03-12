6f01e5b458658e38aafb5bf02f871718f91cb47a
hoceyn mohsenkhah <hoceyn@gmail.com>
2025-03-12 13:57:54 +0330

    [ fix ] Improve Popup component UI and add close button
    Improved the UI of the Popup component by making the content
    width responsive and adding a close button.  The "do not show
    again" checkbox was also improved to use translated text and
    a second button was added allowing the user to navigate to the
    relevant page instead of just closing the popup.  These changes
    enhance the user experience and provide more control.

--- Popup.tsx
+++ Popup.tsx
@@ -1,18 +1,19 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import Button from "./Button";
 import Language from "@/locales/Language";
 import Modal from "./Modal";
 import Image from "./Image";
 import ContentType from "@/types/ContentType";
-import { useRouter } from "next/navigation";
+import {useRouter} from "next/navigation";
+import ColorTypes from "@/components/functions/ColorTypes";
 
 type PopupProps = {
     popups: ContentType;
 };
 
-const Popup: React.FC<PopupProps> = ({ popups }) => {
+const Popup = ({ popups }) => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
     const [doNotShowAgain, setDoNotShowAgain] = useState<boolean>(false);
     const [currentPopup, setCurrentPopup] = useState<any>(null);
@@ -60,7 +61,7 @@ const Popup: React.FC<PopupProps> = ({ popups }) => {
                 <Modal open={isOpen} onClose={handleClose} name="modal-popup" zindex={50}>
                     <div className="py-5 flex flex-col items-center justify-center">
                         <div className="flex flex-col items-center gap-2">
-                            <div onClick={handleNavigate} className="cursor-pointer rounded-md lg:w-96 w-64">
+                            <div onClick={handleNavigate} className="cursor-pointer rounded-md lg:w-96 w-full">
                                 {currentPopup.avatar && (
                                     <Image src={process.env.NEXT_PUBLIC_UPLOAD_URL + "/" + currentPopup.avatar}
                                         alt={currentPopup.title} type="cover" />
@@ -73,22 +74,28 @@ const Popup: React.FC<PopupProps> = ({ popups }) => {
                                 <span onClick={handleNavigate} className=" text-slate-400 cursor-pointer text-sm mt-2">
                                     {currentPopup.short_description}
                                 </span>}
-                            <div className="flex items-center mt-2 gap-1">
-                                <input
-                                    type="checkbox"
-                                    id="doNotShowAgain"
-                                    className="mr-2"
-                                    checked={doNotShowAgain}
-                                    onChange={(e) => setDoNotShowAgain(e.target.checked)}
-                                />
-                                <label htmlFor="doNotShowAgain" className="text-sm text-gray-200">
-                                    دیگر نمایش داده نشود
-                                </label>
-                            </div>
-                            <div className="flex justify-center items-center w-full mt-2 cursor-pointer">
-                                <Button color="default" type="button" onClick={handleClose}>
-                                    {lang.close}
-                                </Button>
+                            <div className="flex justify-between items-center w-full mt-8 cursor-pointer">
+                                <div className="flex items-center gap-1">
+                                    <input
+                                        type="checkbox"
+                                        id="doNotShowAgain"
+                                        className="mr-2"
+                                        checked={doNotShowAgain}
+                                        onChange={(e) => setDoNotShowAgain(e.target.checked)}
+                                    />
+                                    <label htmlFor="doNotShowAgain" className="text-sm text-gray-200">
+                                        {Language().do_not_show_again}
+                                    </label>
+                                </div>
+                                <div className="flex items-center gap-1">
+                                    <Button color={ColorTypes.default} type="button" onClick={handleClose}>
+                                        {lang.close}
+                                    </Button>
+                                    <Button color={ColorTypes.primary} type="button" onClick={handleNavigate}>
+                                        {lang.show}
+                                    </Button>
+                                </div>
+
                             </div>
                         </div>
                     </div>
