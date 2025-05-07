938134644a3261d29f40626966b1de7cce9195c7
Faeze <>
2025-05-07 10:14:53 +0330

    [fix] menu

--- SideMenu.tsx
+++ SideMenu.tsx
@@ -52,51 +52,6 @@ const SideMenu = () => {
 			}}></div>
 			<div className={"sticky w-[70%] bg-slate-900 z-10 max-h-[95vh] overflow-y-auto p-5 py-6 rounded-xl shadow-2xl text-slate-900 text-center "}>
 				<div className={"flex flex-col"}>
-					{/* <div >
-						<a href={"/"}
-							className={"flex flex-col gap-4 items-center justify-center bg-bg-gilace rounded-lg p-4  text-slate-50 active:scale-105 transition-all "}>
-							<span className={"far fa-home text-4xl text-violet-300 opacity-70"}></span>
-							<span>{Language().home}</span>
-						</a>
-					</div>
-					<div className={"grid grid-cols-2 gap-4"}>
-						<a href={"/solutions"}
-							className={"flex flex-col gap-4 items-center justify-center bg-bg-gilace rounded-lg p-4  text-slate-50 active:scale-105 transition-all"}>
-							<span className={"far fa-rocket text-4xl text-violet-300 opacity-70"}></span>
-							<span>{Language().solutions}</span>
-						</a>
-						<a href={"/projects"}
-							className={"flex flex-col gap-4 items-center justify-center bg-bg-gilace rounded-lg p-4  text-slate-50 active:scale-105 transition-all "}>
-							<span className={"far fa-coffee text-4xl text-violet-300 opacity-70"}></span>
-							<span>{Language().projects}</span>
-						</a>
-					</div>
-					<div >
-						<a href={"/auth"}
-								className={"flex flex-col gap-4 items-center justify-center bg-bg-gilace rounded-lg p-4  text-slate-50 active:scale-105 transition-all "}>
-								<span className={"far fa-user-crown text-4xl text-violet-300 opacity-70"}></span>
-								<span>{isHydrated && isAuthenticated ? Language().profile : Language().login_to_panel}</span>
-							</a>
-					</div>
-					<div className={"grid grid-cols-2 gap-4"}>
-						<a href={"/about"}
-							className={"flex flex-col gap-4 items-center justify-center bg-bg-gilace rounded-lg p-4  text-slate-50 active:scale-105 transition-all "}>
-							<span className={"far fa-crown text-4xl text-violet-300 opacity-70"}></span>
-							<span>{Language().about_us}</span>
-						</a>
-						<a href={"/contact"}
-							className={"flex flex-col gap-4 items-center justify-center bg-bg-gilace rounded-lg p-4  text-slate-50 active:scale-105 transition-all "}>
-							<span className={"far fa-phone text-4xl text-violet-300 opacity-70"}></span>
-							<span>{Language().contact}</span>
-						</a>
-					</div>
-					<div>
-						<a href={"/blog"}
-							className={"flex flex-col gap-4 items-center justify-center bg-bg-gilace rounded-lg p-4  text-slate-50 active:scale-105 transition-all "}>
-							<span className={"far fa-blog text-4xl text-violet-300 opacity-70"}></span>
-							<span>{Language().blog}</span>
-						</a>
-					</div> */}
 					<div className="flex flex-col justify-center gap-2 items-center">
 						<div className="rounded-full w-20 h-20">
 							{
@@ -118,62 +73,61 @@ const SideMenu = () => {
 							}
 						</div>
 						<Link href={"/auth"}>
-							<span className="text-indigo-700 border-b  text-sm font-medium">
+							<span onClick={() => MenuStores.setMenu(false)} className="text-indigo-700 border-b  text-sm font-medium">
 								{loading ? <Loader /> : isHydrated && isAuthenticated ? user?.name : Language().login}
 							</span>
 						</Link>
 						<Link href={"/auth"}>
-							<span className="text-slate-400  text-xs font-base ">
+							<span onClick={() => MenuStores.setMenu(false)} className="text-slate-400  text-xs font-base ">
 								{loading ? <Loader /> : isHydrated && isAuthenticated ? user?.username : Language().do_login}
 							</span>
 						</Link>
 					</div>
 					<div className="bg-slate-800 rounded-4xl p-5 grid grid-cols-2 mt-8 justify-center items-center gap-4">
 						<Link href={isHydrated && isAuthenticated ? '' : "/auth"}>
-							<div className="w-18 h-18 rounded-full flex justify-center items-center bg-slate-950 m-auto">
+							<div onClick={() => MenuStores.setMenu(false)} className="w-18 h-18 rounded-full flex justify-center items-center bg-slate-950 m-auto">
 								<span className="far fa-user-cog  text-2xl text-indigo-700">
 								</span>
 							</div>
 						</Link>
 						<Link href={'/'}>
-							<div className="w-18 h-18 rounded-full flex justify-center items-center bg-slate-200 m-auto">
+							<div onClick={() => MenuStores.setMenu(false)} className="w-18 h-18 rounded-full flex justify-center items-center bg-slate-200 m-auto">
 								<span className="far fa-home text-2xl text-indigo-700">
 								</span>
 							</div>
 						</Link>
 						<Link href={'/about'}>
-							<div className="w-18 h-18 rounded-full flex justify-center items-center bg-slate-950 m-auto">
+							<div onClick={() => MenuStores.setMenu(false)} className="w-18 h-18 rounded-full flex justify-center items-center bg-slate-950 m-auto">
 								<span className="far fa-people-group text-2xl text-indigo-700">
 								</span>
 							</div>
 						</Link>
 						<Link href={'/contact'}>
-							<div className="w-18 h-18 rounded-full flex justify-center items-center bg-slate-950 m-auto">
+							<div onClick={() => MenuStores.setMenu(false)} className="w-18 h-18 rounded-full flex justify-center items-center bg-slate-950 m-auto">
 								<span className="far fa-phone text-2xl text-indigo-700">
 								</span>
 							</div>
 						</Link>
 					</div>
 					<Link href={"/blog"}>
-						<div className="text-slate-200 bg-slate-800 rounded-4xl p-3 flex mt-3 justify-center items-center gap-2">
+						<div onClick={() => MenuStores.setMenu(false)} className="text-slate-200 bg-slate-800 rounded-4xl p-3 flex mt-3 justify-center items-center gap-2">
 							<i className="fa-solid fa-blog "></i>
 							<span className="tetx-sm font-semibold">{Language().blog}</span>
 						</div>
 					</Link>
 					<Link href={"/solutions"}>
-						<div className="text-slate-200 bg-slate-800 rounded-4xl p-3 flex mt-3 justify-center items-center gap-2">
+						<div onClick={() => MenuStores.setMenu(false)} className="text-slate-200 bg-slate-800 rounded-4xl p-3 flex mt-3 justify-center items-center gap-2">
 							<i className="far fa-lightbulb-on "></i>
 							<span className="tetx-sm font-semibold">{Language().solutions}</span>
 						</div>
 					</Link>
 					<Link href={"/projects"}>
-						<div className="text-slate-200 bg-slate-800 rounded-4xl p-3 flex mt-3 justify-center items-center gap-2">
+						<div onClick={() => MenuStores.setMenu(false)} className="text-slate-200 bg-slate-800 rounded-4xl p-3 flex mt-3 justify-center items-center gap-2">
 							<i className="far fa-diamond "></i>
 							<span className="tetx-sm font-semibold">{Language().projects}</span>
 						</div>
 					</Link>
-					<div className=" grid grid-cols-2 mt-8 justify-center items-center gap-3">
-
+					{/* <div className=" grid grid-cols-2 mt-8 justify-center items-center gap-3">
 						<div className="bg-slate-800 rounded-3xl justify-center items-center w-24 h-24 flex flex-col gap-4">
 							<Image src={gclubicon} alt="gclub" />
 							<span className="text-slate-200 text-sm font-medium">
@@ -186,7 +140,6 @@ const SideMenu = () => {
 								Ai Mod
 							</span>
 						</div>
-
 						<div className="bg-slate-800 rounded-3xl justify-center items-center w-24 h-24 flex flex-col gap-4">
 							<Image src={barcodeicon} alt="gshape" />
 							<span className="text-slate-200 text-sm font-medium">
@@ -199,7 +152,7 @@ const SideMenu = () => {
 								G-Shape
 							</span>
 						</div>
-					</div>
+					</div> */}
 				</div>
 			</div>
 			<div
